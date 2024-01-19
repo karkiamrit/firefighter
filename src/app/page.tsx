db19080ad5464378adf5e3f9bdcb338c89ab3a6f
iam8uman <iam8uman@gmail.com>
2024-01-19 21:24:54 +0545

    Navbar and its items updated completely

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import Partners from '@/components/Partners';
 import Navbar from '@/components/static/Navbar';
 import Footer from '@/components/Footer';
 import LandingPage from '@/components/static/LandingPage';
+import MaxWidthWrapper from '@/components/wrapper/MaxWidthWrapper';
+import TopEvent from '@/components/TopEvent';
 
 export default function Home() {
   return (
@@ -12,6 +14,9 @@ export default function Home() {
       <div className="relative w-full bg-muted py-5">
         <Features />
       </div>
+      <MaxWidthWrapper>
+        <TopEvent />
+      </MaxWidthWrapper>
 
       <div className="relative w-full bg-light">
         <Partners />
